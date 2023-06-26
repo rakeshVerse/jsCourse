89640e3a89fdf4844a2109763154a641df5e2803
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-26 12:56:56 +0530

    Add function returning function

---
+++ functions/function_return.js
@@ -0,0 +1,27 @@
+/**
+ * Functions that returns another function are Higher Order Functions
+ */
+
+/**
+ * Normal function example
+ */
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting}, ${name}!!!`);
+  };
+};
+
+// two ways of calling:
+// 1st
+const greeter = greet("Hi"); // greet() returns a function and store it in a variable 'greeter'
+greeter("John"); // since, greeter holds returned function, we can call it with argument -> Hi, John!!!
+
+// 2nd
+// Syntax: higherOrderFunction(args)(returnedFunctionArgs)
+greet("Hello")("Ram"); // -> Hello, Ram!!!
+
+/**
+ * Arrow function example
+ */
+const greetMe = (greeting) => (name) => console.log(`${greeting}, ${name}!!!`);
+greetMe("Hey")("Bob"); // -> Hey, Bob!!!
