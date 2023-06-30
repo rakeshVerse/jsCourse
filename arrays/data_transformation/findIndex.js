3c51ba9b55de7ef6eb072e3f680908abc7377251
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-06-30 13:00:21 +0530

    Add find index method

---
+++ arrays/data_transformation/findIndex.js
@@ -0,0 +1,10 @@
+/**
+ * findIndex() returns the index of array element that satisfies given condition
+ *
+ * It loops over the array and calls a Callback function for each element
+ *
+ * It is similar to indexOf(). The difference is, in indexOf(element) we have to pass element as attribute
+ * While in findIndex(), we pass a Callback that checks for given condition
+ *
+ * Syntax: array.findIndex(function(item, index){})
+ */
