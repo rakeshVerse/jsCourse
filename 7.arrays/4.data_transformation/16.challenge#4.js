221fa99e48bffd298cc55e1ed25b88effdf9c8ad
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-02 18:58:11 +0530

    Challenge 4

--- 7.arrays/4.data_transformation/16.challenge#4.js
+++ 7.arrays/4.data_transformation/16.challenge#4.js
@@ -32,49 +32,47 @@ const dogs = [
 ];
 
 // 1.
-dogs.forEach(function (dog) {
-  dog.recommendedFood = dog.weight ** 0.75 * 28;
-});
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
 
-// console.log(dogs);
+console.log(dogs);
 
 // 2.
-const sarahDog = dogs.find(function (dog) {
-  return dog.owners.includes('Sarah');
-});
-const eatingSize =
-  sarahDog.curFood > sarahDog.recommendedFood
-    ? 'Eating too much'
-    : 'Eating too little';
-// console.log(eatingSize);
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(sarahDog);
+console.log(
+  `Sarah's dog is eating too ${
+    sarahDog.curFood > sarahDog.recFood ? 'much' : 'little'
+  }`
+);
 
 // 3.
 const ownersEatTooMuch = dogs
-  .filter(dog => {
-    if (dog.curFood > dog.recommendedFood) return dog.owners;
-  })
+  .filter(dog => dog.curFood > dog.recFood)
   .flatMap(dog => dog.owners);
-console.log(ownersEatTooMuch);
+
 const ownersEatTooLittle = dogs
-  .filter(dog => {
-    if (dog.curFood < dog.recommendedFood) return dog.owners;
-  })
+  .filter(dog => dog.curFood < dog.recFood)
   .flatMap(dog => dog.owners);
-console.log(ownersEatTooLittle);
+
+console.log(ownersEatTooMuch, ownersEatTooLittle);
 
 // 4.
 console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
 console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
 
 // 5.
-console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
 
 // 6.
-const okFood = dogs.some(dog => {
-  dog.curFood > dog.recommendedFood * 0.9 &&
-    dog.curFood < dog.recommendedFood * 1.1;
-});
-
-console.log(okFood);
+const checkOkFood = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkOkFood));
 
 // 7.
+const okFoodDogs = dogs.filter(checkOkFood);
+console.log(okFoodDogs);
+
+// 8.
+const dogsCp = [...dogs].sort((a, b) => a.recFood - b.recFood);
+console.log(dogsCp);
