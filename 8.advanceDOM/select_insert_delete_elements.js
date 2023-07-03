c3ddfb4ef928f4b6422684f68b70e207349c14e3
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-03 18:09:25 +0530

    Add style, attribute and class manupulation

--- 8.advanceDOM/select_insert_delete_elements.js
+++ 8.advanceDOM/select_insert_delete_elements.js
@@ -1,3 +1,7 @@
+/**
+ * Select, Insert & Delete elements
+ */
+
 // SELECTING ELEMENTS
 console.log(document.documentElement); // selects entire document
 console.log(document.head);
@@ -34,24 +38,24 @@ document
   .querySelector('.sub-txt')
   .insertAdjacentHTML('afterbegin', `<h2>This is a sub heading</h2>`);
 
-message.textContent =
-  'We use cookied for improved functionality and analytics.';
+// message.textContent =
+//   'We use cookied for improved functionality and analytics.';
 
 message.innerHTML =
   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
 // inserts at the top inside the element
 // e.g. < h1 > <div class='cookie-message'></div> Heading</ >
-header.prepend(message);
+// header.prepend(message);
 
 // inserts at the bottom inside the element
-header.append(message); // inserts after header text
+// header.append(message); // inserts after header text
 
 // there will be only 1 'message' on the page
 // To have duplicate 'message' we need to create its clone
-header.append(message.cloneNode(true)); // creates clone of element 'message' & appends it
+// header.append(message.cloneNode(true)); // creates clone of element 'message' & appends it
 
-header.before(message); // adds element before header e.g. <div class='cookie-message'></div> < h1 > Heading</ >
+// header.before(message); // adds element before header e.g. <div class='cookie-message'></div> < h1 > Heading</ >
 header.after(message); // adds element after header
 
 // DELETE ELEMENTS
@@ -61,3 +65,71 @@ document
     message.remove(); // new method - directly remove element
     // message.parentElement.removeChild(message); // old way - traverse to parent then remove child
   });
+
+// ///////////////////////////////////////////////////////
+
+/**
+ * Manipulating Styles, Attribues & Classes
+ */
+
+// STYLE
+
+// Adding styles dynamically (inline)
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+// Reading styles
+// reading dynamically added styles
+console.log(message.style.backgroundColor);
+console.log(message.style.width);
+
+// reading static styles
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+// read existing height to set new height
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// manupulate css root variables
+// Syntax: document.documentElement.style.setProperty(variable, value);
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// ATTRIBUTES
+
+const img = document.querySelector('.head-img');
+
+// Read/Get attributes
+console.log(img.alt); // -> grow
+console.log(img.className); // -> head-img
+
+// Add/change attribute
+img.alt = 'Beautiful Plant';
+
+// Reading Non-standard
+console.log(img.designer); // doesn't work
+console.log(img.getAttribute('designer')); // rr
+
+console.log(img.src); // returns absolute path -> http://127.0.0.1:8080/grow.jpg
+console.log(img.getAttribute('src')); // returns value of src -> grow.jpg
+
+const link = document.querySelector('.link');
+console.log(link.href); // returns absolute path -> http://127.0.0.1:8080/grow.jpg
+console.log(link.getAttribute('href')); // returns value of href -> grow.jpg
+
+// Setting attributes
+img.setAttribute('company', 'Banker');
+
+// Data attributes
+console.log(img.dataset.versionNumber);
+
+// CLASSES
+
+// add class
+img.className = 'greenplant'; // adds specified class by overwriting all the existing classes
+img.classList.add('class1', 'class2'); // append new class comma sepreated
+img.classList.remove('class1', 'class2'); // remove classes comma sepreated
+img.classList.toggle('class1'); // add class if not exist, remove if exist
+console.log(img.classList.contains('class1')); // check if element contains specified class, returns true/false
+
+// //////////////////////////////////
