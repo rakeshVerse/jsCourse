dd89a09cce7fc17e089edec37bbae4e0b639105e
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-13 19:13:06 +0530

    Refactore promise chain

--- 11.async/3.promise/7.refactoring/script.js
+++ 11.async/3.promise/7.refactoring/script.js
@@ -9,7 +9,7 @@ const load = document.querySelector('.load');
 const renderError = msg =>
   countriesContainer.insertAdjacentText(
     'beforeend',
-    `Something went wrong: ${msg}`
+    `An error occurred! ${msg}.`
   );
 
 const renderCountry = (country, className = '') => {
@@ -36,52 +36,39 @@ const renderCountry = (country, className = '') => {
   countriesContainer.insertAdjacentHTML('beforeend', html);
 };
 
-// THROWING ERRORS MANUALLY
+// REFACTORING PROMISE CHAIN
 
-// Errors can occure when making Ajax calls
-// On user end, user may lose the internet connnection
-// While on server end, Server may return an error in case, e.g. requested data not found or server is down, etc.
-
-// Suppose data user requested is not found (i.e. looking for country that doesn't exists).
-// In, this case promise will still resolve to Fullfilled.
-// In such case, handling the error using catch() won't show the actual error which is in this case (Country not found)
-// We need to throw the errors manually.
-// throwing error will reject the promise chain and execute the catch()
-
-// Throwing 3 errors:
-// 1. When first Ajax doesn't find country i.e. resolves with .ok = false
-// 2. When second Ajax doesn't find country i.e.resolves with .ok = false
-// 3. When country doesn't have bordering countries e.g. australia
+const getCountryJSON = function (resource, errMsg) {
+  return fetch(`https://restcountries.com/v3.1/${resource}`).then(res => {
+    if (!res.ok) throw new Error(`${errMsg} (${res.status})`);
+    return res.json();
+  });
+};
 
 const getContryAndNeighbours = function (country) {
   // Ajax call 1: get country
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(res => {
-      // Throw an error if res.ok is false
-      // throw will reject the entire promise chain & send control to catch()
-      if (!res.ok) throw new Error(`Country not found (${res.status})`);
-
-      return res.json();
-    })
-    .then(res => {
-      renderCountry(res[0]);
+  getCountryJSON(
+    `name/${country}`,
+    `Country named ${country.toUpperCase()} not found`
+  )
+    .then(response => {
+      renderCountry(response[0]);
 
       // Get neighbours country code
-      let neighboursCode = res[0].borders ? res[0].borders.join(',') : null;
+      let neighboursCode = response[0].borders
+        ? response[0].borders.join(',')
+        : null;
       if (!neighboursCode)
-        throw new Error(`${country} doesn't share border with any country`);
+        throw new Error(
+          `${country.toUpperCase()} doesn't share border with any country`
+        );
 
       // To generate an error, lets search for some random code
-      neighboursCode = 'asdfsde';
+      // neighboursCode = 'asdfsde';
 
       // Ajax call 2: get neighbour countries using their country codes
       // prettier-ignore
-      return fetch(`https://restcountries.com/v3.1/alpha?codes=${neighboursCode}`);
-    })
-    .then(res => {
-      // Throw an error if res.ok is false
-      if (!res.ok) throw new Error(`Country not found (${res.status})`);
-      return res.json();
+      return getCountryJSON(`alpha?codes=${neighboursCode}`, 'Country with given code not found');
     })
     .then(neighbours =>
       neighbours.forEach(neighbour => renderCountry(neighbour, 'neighbour'))
@@ -91,13 +78,16 @@ const getContryAndNeighbours = function (country) {
     })
     .finally(() => {
       countriesContainer.style.opacity = 1; // unhide container
-      load.style.opacity = 0; // hide loader
+      load.style.display = 'none'; // hide loader
+      btn.style.display = 'none'; // hide button
     });
 };
 
 btn.addEventListener('click', function () {
   load.style.opacity = 1;
   getContryAndNeighbours('india');
+  // getContryAndNeighbours('congo');
+  // getContryAndNeighbours('chile');
+  // getContryAndNeighbours('turkey');
   // getContryAndNeighbours('australia');
-  // getContryAndNeighbours('brazil');
 });
