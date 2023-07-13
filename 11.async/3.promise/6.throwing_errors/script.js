203e59c4f2e8f648620395fe7ac7a24e806705c7
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-13 18:01:46 +0530

    Add throw error functionality

--- 11.async/3.promise/6.throwing_errors/script.js
+++ 11.async/3.promise/6.throwing_errors/script.js
@@ -42,33 +42,51 @@ const renderCountry = (country, className = '') => {
 // On user end, user may lose the internet connnection
 // While on server end, Server may return an error in case, e.g. requested data not found or server is down, etc.
 
-// Suppose data user requested is not found. In, this case promise will still resolve to Fullfilled.
-// In such case, we can't handle the error using catch()
-// We need to throw the errors manually
+// Suppose data user requested is not found (i.e. looking for country that doesn't exists).
+// In, this case promise will still resolve to Fullfilled.
+// In such case, handling the error using catch() won't show the actual error which is in this case (Country not found)
+// We need to throw the errors manually.
 // throwing error will reject the promise chain and execute the catch()
+
+// Throwing 3 errors:
+// 1. When first Ajax doesn't find country i.e. resolves with .ok = false
+// 2. When second Ajax doesn't find country i.e.resolves with .ok = false
+// 3. When country doesn't have bordering countries e.g. australia
 
 const getContryAndNeighbours = function (country) {
   // Ajax call 1: get country
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(res => res.json())
+    .then(res => {
+      // Throw an error if res.ok is false
+      // throw will reject the entire promise chain & send control to catch()
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
     .then(res => {
       renderCountry(res[0]);
 
       // Get neighbours country code
-      const neighboursCode = res[0].borders ? res[0].borders.join(',') : null;
-      if (!neighboursCode) return;
+      let neighboursCode = res[0].borders ? res[0].borders.join(',') : null;
+      if (!neighboursCode)
+        throw new Error(`${country} doesn't share border with any country`);
+
+      // To generate an error, lets search for some random code
+      neighboursCode = 'asdfsde';
 
       // Ajax call 2: get neighbour countries using their country codes
       // prettier-ignore
       return fetch(`https://restcountries.com/v3.1/alpha?codes=${neighboursCode}`);
     })
-    .then(res => (res ? res.json() : []))
+    .then(res => {
+      // Throw an error if res.ok is false
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
     .then(neighbours =>
       neighbours.forEach(neighbour => renderCountry(neighbour, 'neighbour'))
     )
     .catch(err => {
-      console.log(err.message); // -> NetworkError when attempting to fetch resource.
-
       renderError(err.message);
     })
     .finally(() => {
@@ -79,7 +97,7 @@ const getContryAndNeighbours = function (country) {
 
 btn.addEventListener('click', function () {
   load.style.opacity = 1;
-  getContryAndNeighbours('indiaaaaa');
+  getContryAndNeighbours('india');
   // getContryAndNeighbours('australia');
   // getContryAndNeighbours('brazil');
 });
