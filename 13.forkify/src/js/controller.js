8f17bd7328974660ae028fb854967313ce4f1b60
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-20 17:23:35 +0530

    Initial commit: Initialize NPM, Parcel, Create NPM scripts, Hot reload

---
+++ 13.forkify/src/js/controller.js
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////
+// hot reload
+if (module.hot) {
+  module.hot.accept();
+}
+
+/////////////////////////////////////////////////////
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
