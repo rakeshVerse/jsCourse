124397d267e5c882dc61c11b96e6b5225eea5189
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-07-25 16:38:37 +0530

    Change request timeout seconds

--- 13.forkify/src/js/config.js
+++ 13.forkify/src/js/config.js
@@ -1,6 +1,6 @@
 // Variables shared across the App
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TIMEOUT_SEC = 10;
+export const TIMEOUT_SEC = 20;
 export const RES_PER_PAGE = 10;
 export const KEY = 'd9fb26db-b1f9-4f6e-bf3b-3ee95189ca37';
 export const CLOSE_TIMEOUT = 1;
