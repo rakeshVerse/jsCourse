a8573c8daea4ba7e7766a161fdfb5c77f0d7c464
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-09-09 15:21:05 +0530

    Add challenge description

--- 3.debugging/challenge#1.js
+++ 3.debugging/challenge#1.js
@@ -1,8 +1,21 @@
 /**
- * Create a function which take an array as parameter and returns a string
+ * 
+ * Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+
+Test data:
+§Data 1: [17, 21, 23]
+§Data 2: [12, 5, -5, 0, 4]
  */
 
-const printForecast = (array) => {
+const printForecast = array => {
   let forecast = `...`;
   for (let i = 0; i < array.length; i++) {
     forecast += ` ${array[i]}C in ${i + 1} Days ...`;
