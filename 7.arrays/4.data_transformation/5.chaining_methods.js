5c24b3f4e847e02af4f0671bd3f0166555199457
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-10-03 14:53:50 +0530

    Add solution using reduce() only

--- 7.arrays/4.data_transformation/5.chaining_methods.js
+++ 7.arrays/4.data_transformation/5.chaining_methods.js
@@ -3,7 +3,7 @@
 // 3. Calculate total balance
 
 const currUSD = [120, 457, -99, 487, -58, -97, 855, -9, 897];
-const convertToINR = 82.01;
+const exchangeRate = 82.01;
 
 const balanceINR = (transac, conversionRate) =>
   transac
@@ -11,4 +11,16 @@ const balanceINR = (transac, conversionRate) =>
     .map(amt => amt * conversionRate)
     .reduce((acc, amt) => acc + amt, 0);
 
-console.log(balanceINR(currUSD, convertToINR));
+console.log(balanceINR(currUSD, exchangeRate));
+
+// Solving using reduce() only
+
+const totalBalanceINR = currUSD.reduce((total, amount) => {
+  if (amount > 0) {
+    return total + amount * exchangeRate;
+  } else {
+    return total;
+  }
+}, 0);
+
+console.log(`Total balance in INR: ${totalBalanceINR}`);
