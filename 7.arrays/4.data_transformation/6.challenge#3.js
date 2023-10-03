9314d66c4a2df21d00551fadd821e6dcd8507ab6
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-10-03 16:24:23 +0530

    Add reduce() only solution

--- 7.arrays/4.data_transformation/6.challenge#3.js
+++ 7.arrays/4.data_transformation/6.challenge#3.js
@@ -5,6 +5,8 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 */
+
+// Solution 1: Method chaining
 const calcAverageHumanAge = dogsAge =>
   dogsAge
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
@@ -13,3 +15,24 @@ const calcAverageHumanAge = dogsAge =>
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])); // -> 44
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])); // -> 47.33
+
+// Solution 2: reduce() only
+const calcAverageHumanAgeReduce = arr => {
+  const { sum, count } = arr.reduce(
+    ({ sum, count }, age) => {
+      if (age > 2) {
+        const humanAge = 16 + age * 4;
+        return humanAge >= 18
+          ? { sum: sum + humanAge, count: count + 1 }
+          : { sum, count };
+      }
+
+      return { sum, count };
+    },
+    { sum: 0, count: 0 }
+  );
+  return `Average human age: ${sum / count}`;
+};
+
+console.log(calcAverageHumanAgeReduce([5, 2, 4, 1, 15, 8, 3])); // -> 44
+console.log(calcAverageHumanAgeReduce([16, 6, 10, 5, 6, 1, 4])); // -> 47.33
